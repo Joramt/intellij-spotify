c85be3c5e2c835dc7ec8ce3f22b35d48f4834f86
GitHub Action <action@github.com>
2025-06-09 00:53:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/joramt/intellijspotify/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/joramt/intellijspotify/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.joramt.intellijspotify.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
